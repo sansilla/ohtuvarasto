e33e2c4d8fff9254136db9cf7a7e0df948e0a858
Sandra Sillanpää <sandra.sillanpaa@helsinki.fi>
2024-11-01 14:09:04 +0200

    korjaus taas

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -8,7 +8,7 @@ class TestVarasto(unittest.TestCase):
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
-        self.assertAlmostEqual(self.varasto.saldo, 2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_vaara_tilavuus(self):
         self.varasto = Varasto(-1)
